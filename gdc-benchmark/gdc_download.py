94134dfabe9dc69867c5f959f25580e79502fc0f
Aécio Santos <aecio.solando@gmail.com>
2025-04-28 17:53:43 -0400

    Add cleaning steps to ensure CSVs as like the ones used in the paper
    After running all experiments and finalizing the paper, we
    noticed that the tables used to run experiments are slightly
    different from the tables published as supplemental material.
    While we would prefer to publish the raw data, to ensure full
    reproducility and transparency, we (the paper authors) decided
    to include these data cleaning steps here to make sure that the
    same files used in our experiments can be recreated.
    We will consider removing some of these cleaning steps in a
    future release of the dataset.

--- gdc-benchmark/gdc_download.py
+++ gdc-benchmark/gdc_download.py
@@ -20,12 +20,12 @@ def extract_csv_from_excel(excel_file_name, sheet_name):
         df = pd.read_excel(excel_file_name, sheet_name=sheet_name)
         return df
     except Exception as e:
-        print(f"Error extracting sheet {sheet_name} from {
-              excel_file_name}: {str(e)}")
+        print(f"Error extracting sheet {sheet_name} from {excel_file_name}: {str(e)}")
         return None
 
 
 for paper in papers_info:
+    print(f"Processing {paper}:")
     dataset_url = papers_info[paper]['Dataset URL']
     if dataset_url:
         response = requests.get(dataset_url)
@@ -43,8 +43,26 @@ for paper in papers_info:
                 df = extract_csv_from_excel(download_file_name, sheet_name)
 
                 if paper == "Vasaikar.csv":
-                    # remove incompatilhe rows in Vasaiker dataset
+                    # remove incompatible rows in Vasaikar dataset
                     df = df.iloc[1:-1]
+
+                if paper == "McDermott.csv":
+                    # remove tab characters from column names
+                    df.rename(columns={
+                        "Age in Months at Time of Tissue Procurement \t": "Age in Months at Time of Tissue Procurement",
+                    }, inplace=True)
+
+                if paper == "Clark.csv":
+                    # fix typo in values "Whte" to "White" in column Ethnicity_Self_Identify
+                    if 'Ethnicity_Self_Identify' not in df.columns:
+                        raise ValueError(f'Expected column Ethnicity_Self_Identify but not found in table from paper {paper}')
+                    df['Ethnicity_Self_Identify'] = df['Ethnicity_Self_Identify'].replace("Whte", "White")
+
+                if paper == "Wang.csv":
+                    # clean values in column "secondhand_smoke_exposure"
+                    df['secondhand_smoke_exposure'] = df['secondhand_smoke_exposure'].replace("No or minimal exposure to secondhand smoke", "No")
+                    df['secondhand_smoke_exposure'] = df['secondhand_smoke_exposure'].replace("Exposed in childhood houshold", "Yes")
+                    df['secondhand_smoke_exposure'] = df['secondhand_smoke_exposure'].replace("Exposed in current household", "Yes")
 
                 if df is not None:
                     csv_file_name = os.path.join(prepared_folder, paper)
@@ -52,3 +70,4 @@ for paper in papers_info:
                     print(f"Created CSV file: {csv_file_name}")
         else:
             print(f"Failed to download {dataset_url}")
+    print("")
